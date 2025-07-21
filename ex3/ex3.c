ce19de1d19404b1cd5f53cc7b5fcedcb3e3e74e4
Meridian D Play <meridian.d.play@gmail.com>
2025-07-21 22:10:46 +0200

    Exercise 1 to 3

---
+++ ex3/ex3.c
@@ -0,0 +1,18 @@
+/*
+ * Exercise 3
+ * Formatted Printing
+ *
+ */
+
+#include <stdio.h>
+
+int main (void) {
+
+  int age = 10;
+  int height = 72;
+
+  printf("I am %d years old.\n", age);
+  printf("i am %d inches tall.\n", height);
+
+  return 0;
+}
